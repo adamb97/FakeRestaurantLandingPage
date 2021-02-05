6db43ad70f85efcfafc73453ba7b6ebd418b299c
adam <adben97@gmail.com>
2021-02-04 19:19:11 -0500

    added todos

--- RestaurantLandingPage/menu.js
+++ RestaurantLandingPage/menu.js
@@ -1,26 +1,29 @@
-const carouselSlide = document.querySelector(".slides");
-const carouselImgs = document.querySelectorAll(".slides img");
+// const carouselSlide = document.querySelector(".slides");
+// const carouselImgs = document.querySelectorAll(".slides img");
 
-// buttons
+// // buttons
 
-const previousBtn = document.querySelector("#prevBtn");
-const nextBtn = document.querySelector("#nextBtn");
+// const previousBtn = document.querySelector("#prevBtn");
+// const nextBtn = document.querySelector("#nextBtn");
 
-// counter
+// // counter
 
-let currentImgCounter = 1;
-const size = carouselImgs[0].clientWidth;
+// let currentImgCounter = 1;
+// const size = carouselImgs[0].clientWidth;
 
-carouselSlide.style.transform =
-  "translateX(" + -size * currentImgCounter + "px)";
+// carouselSlide.style.transform =
+//   "translateX(" + -size * currentImgCounter + "px)";
 
-// listeners
+// // listeners
 
-nextBtn.addEventListener("click", () => {
-  carouselSlide.style.transition = "transform 0.4 ease-in-out";
-  currentImgCounter++;
-  carouselSlide.style.transform =
-    "translateX(" + -size * currentImgCounter + "px)";
+// nextBtn.addEventListener("click", () => {
+//   carouselSlide.style.transition = "transform 0.4 ease-in-out";
+//   currentImgCounter++;
+//   carouselSlide.style.transform =
+//     "translateX(" + -size * currentImgCounter + "px)";
 
-  console.log(size);
-});
+//   console.log(size);
+// });
+
+
+//TODO refactor for jquery
